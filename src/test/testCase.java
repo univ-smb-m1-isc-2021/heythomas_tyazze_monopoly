00b7e4fa25877f4a2f81e82c9871743b854d3a78
Benjamin <tyazze@users.noreply.github.com>
2022-01-17 16:55:05 +0100

    Update testCase.java

--- src/test/testCase.java
+++ src/test/testCase.java
@@ -3,5 +3,15 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class testCase {
+	@Test
+    void suivant() {
 
+        Kase premKase = new Kase();
+        Kase secdKase = new Kase();
+
+        premKase.suivant = secdKase
+
+        assertThat(premKase.suivant())
+                .isEqualTo(secdKase);
+    }
 }
